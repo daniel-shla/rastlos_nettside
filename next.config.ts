7f6fe41b9af0032eb123e105c35964d8d35e1008
daniel-shla <danielshahzadlandsverk@gmail.com>
2025-03-08 03:10:29 +0100

    Revert changes to workflow and configuration files

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
-  basePath: '',
-  trailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
